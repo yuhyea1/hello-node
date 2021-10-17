0d3bdb44b1de9f96d32385ff49c48713f951b8bc
yuhyea1 <yuhyea1@gmail.com>
2021-10-17 10:49:30 +0800

    上傳promise版本

---
+++ crawler/app.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+let stockCode = "1101";
+let today = "20211015";
+let format = "json";
+
+
+// `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=${format}&date=${today}&stockNo=${stockCode}`
+// 把原本網址的參數用變數去接他，然後再由變數去轉換這樣就可以切換了
+axios
+  .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+    params: {
+      response: format,
+      date: today,
+      stockNo: stockCode,
+    },
+  })
+  .then((res) => {
+    // HTTP response
+    console.log(res.data);
+  })
+  .catch((e) => {
+    console.error("出不來爆炸拉", e);
+  });
