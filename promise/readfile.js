776903b4449497fdcddafb20370a820c986d9e4b
yuhyea1 <yuhyea1@gmail.com>
2021-10-18 14:17:11 +0800

    1018號修改

--- promise/readfile.js
+++ promise/readfile.js
@@ -1,36 +1,31 @@
 // const { rejects } = require("assert");
 const fs = require("fs");
 
-// readFile('input.txt', (err, data) => {
-// //   if (err) {
-// //       console.error("發生錯誤", err);
-// //   } else {
-// //       console.log("正確讀到", data);
-// //   }
+// fs.readFile('input.txt','utf-8', (err, data) => {
+//   if (err) {
+//       console.error("發生錯誤", err);
+//   } else {
+//       console.log("正確讀到", data);
+//   }
 // });
 
 function test(){
     return new Promise((resolve,reject)=>{
-        fs.readFile("input.txt", "utf-8", (err,data)=>{
-
+        fs.readFile('input.txt','utf-8', (err, data) => {
             if (err) {
-                // console.error("發生錯誤", err);
-                reject(err);
+               reject(err);
             } else {
-                // console.log("正確讀到", data);
                 resolve(data);
             }
-        })
-    });
-
+          });
+    })
 }
-test().then((data)=>{
-    console.log("正確讀到", data);
-})
-
-.catch((err)=>{
-    console.error("發生錯誤", err);
-});
+    let a = test();
+    a.then((data)=>{
+        console.log("你成功了阿哈哈哈",data)
+    }).catch((err)=>{
+        console.log("可惡",err)
+    })
 
 
 
