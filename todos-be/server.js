25d772298a586ca38314c7ea04ae968714fe2302
yuhyea1 <yuhyea1@gmail.com>
2021-10-29 21:32:57 +0800

    上傳crawler分支

--- todos-be/server.js
+++ todos-be/server.js
@@ -2,14 +2,25 @@ const express = require("express");
 
 let app = express();
 
-app.get("/",(req,res)=>{
-    res.send("我是首頁");
+app.use((req, res, next) => {
+  console.log("我是aaa中間建");
+  next();
 });
 
-app.get("/member",(req,res)=>{
-    res.send("我是會員頁面");
+app.use((req, res, next) => {
+  let current = new Date();
+  console.log(`有人來訪問 at ${current.toISOString()}`);
 });
 
-app.listen(3001,()=>{
-    console.log("express app 啟動了唷")
-})
+app.get("/", (req, res) => {
+  console.log("我是首頁");
+  res.send("我是首頁");
+});
+
+app.get("/member", (req, res) => {
+  res.send("我是會員頁面");
+});
+
+app.listen(3001, () => {
+  console.log("express app 啟動了唷");
+});
